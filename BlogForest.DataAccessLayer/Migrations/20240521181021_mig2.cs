2cd82b2f25633c1b1c970f8a90b0923356ce4483
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-05-21 21:57:20 +0200

    Add project files.

---
+++ BlogForest.DataAccessLayer/Migrations/20240521181021_mig2.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogForest.DataAccessLayer.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Blogs",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_CategoryId",
+                table: "Blogs",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CategoryId",
+                table: "AspNetUsers",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Categories_CategoryId",
+                table: "AspNetUsers",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryId",
+                table: "Blogs",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Categories_CategoryId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryId",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_CategoryId",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CategoryId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ BlogForest.DataAccessLayer/Migrations/20240521181345_mig3.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogForest.DataAccessLayer.Migrations
+{
+    public partial class mig3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Categories_CategoryId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryId",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CategoryId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppUserId",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_AppUserId",
+                table: "Blogs",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_AspNetUsers_AppUserId",
+                table: "Blogs",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryId",
+                table: "Blogs",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_AspNetUsers_AppUserId",
+                table: "Blogs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryId",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_AppUserId",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "Blogs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Blogs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CategoryId",
+                table: "AspNetUsers",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Categories_CategoryId",
+                table: "AspNetUsers",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryId",
+                table: "Blogs",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+    }
+}
